9a815a65ef99e0470f9d351c5a5d5d22f86f741b
Anurak Th <anutech2001@gmail.com>
2020-08-13 09:51:59 +0700

    Add log pertition and offet

--- src/main/java/com/example/kafkaconsumer/service/KafkaConsumerService.java
+++ src/main/java/com/example/kafkaconsumer/service/KafkaConsumerService.java
@@ -2,6 +2,8 @@ package com.example.kafkaconsumer.service;
 
 import com.example.kafkaprovider.model.Event;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,10 +14,11 @@ public class KafkaConsumerService {
     private final Logger logger = LoggerFactory.getLogger(KafkaConsumerService.class);
 
     @KafkaListener(topics = "event", groupId = "myGroup")
-    public void consumeEvent(Event event) {
+    public void consumeEvent(Event event, ConsumerRecord<?, ?> record) {
 
         // logger.info(String.format("Consume event message: '%s'", event));
-        logger.info(event.toString());
+        logger.info(
+                "Partition:" + record.partition() + ", Offset:" + record.offset() + ", Message|" + event.toString());
     }
 
 }
